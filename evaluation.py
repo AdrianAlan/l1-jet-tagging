31b3766ae53c82e6694e1d43536b282ea4137936
Adrian Alan Pol <adrianalan.pol@gmail.com>
2023-03-21 18:14:22 +0200

    Add utils for argparse

--- evaluation.py
+++ evaluation.py
@@ -8,32 +8,7 @@ from itertools import compress
 from pathlib import Path
 from typing import List, Optional
 from tensorflow import keras
-
-
-class IsReadableDir(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        prospective_dir = values
-        if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError(
-                "{0} is not a valid path".format(prospective_dir)
-            )
-        if os.access(prospective_dir, os.R_OK):
-            setattr(namespace, self.dest, prospective_dir)
-        else:
-            raise argparse.ArgumentTypeError(
-                "{0} is not a readable directory".format(prospective_dir)
-            )
-
-
-class IsValidFile(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        prospective_file = values
-        if not os.path.exists(prospective_file):
-            raise argparse.ArgumentTypeError(
-                "{0} is not a valid file".format(prospective_file)
-            )
-        else:
-            setattr(namespace, self.dest, prospective_file)
+from utils import IsReadableDir, IsValidFile
 
 
 def get_dataset(
--- experiment.py
+++ experiment.py
@@ -14,32 +14,7 @@ from sklearn.metrics import auc, roc_curve
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.model_selection import StratifiedKFold
 from typing import List, Optional
-
-
-class IsReadableDir(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        prospective_dir = values
-        if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError(
-                "{0} is not a valid path".format(prospective_dir)
-            )
-        if os.access(prospective_dir, os.R_OK):
-            setattr(namespace, self.dest, prospective_dir)
-        else:
-            raise argparse.ArgumentTypeError(
-                "{0} is not a readable directory".format(prospective_dir)
-            )
-
-
-class IsValidFile(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        prospective_file = values
-        if not os.path.exists(prospective_file):
-            raise argparse.ArgumentTypeError(
-                "{0} is not a valid file".format(prospective_file)
-            )
-        else:
-            setattr(namespace, self.dest, prospective_file)
+from utils import IsReadableDir, IsValidFile
 
 
 def get_dataset(source: Path) -> (np.array, np.array):
